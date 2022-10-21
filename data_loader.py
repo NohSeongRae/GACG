737e3a920f24069e7ef4998ea61e350a099f3da1
SeongraeNoh <rhosunr99@khu.ac.kr>
2022-10-21 13:58:02 +0900

    model encoder: GAT decoder: geometric, semantic based distribution regression loss: semantic distribution, building wise distance sub model: block-net geometric loss

--- data_loader.py
+++ data_loader.py
@@ -1,33 +1,29 @@
-import urllib.request
 import pandas as pd
 import dgl
 from dgl.data import DGLDataset
 import torch
 import os
 
-urllib.request.urlretrieve(
-    'https://data.dgl.ai/tutorial/dataset/members.csv', './members.csv')
-urllib.request.urlretrieve(
-    'https://data.dgl.ai/tutorial/dataset/interactions.csv', './interactions.csv')
+members = pd.read_csv('./SCG_Dataset/OSMNode/LasVegas.csv', low_memory=False, encoding='cp949')  # low_memory false due to mixed data type
+# print(members.head())  # check node csv file
 
-members = pd.read_csv('./members.csv')
-members.head()
+interactions = pd.read_csv('./SCG_Dataset/OSMEdge/Edge_LasVegas.csv', low_memory=False, encoding='cp949')
+# print(interactions.head())  # check edge csv file
 
-interactions = pd.read_csv('./interactions.csv')
-interactions.head()
 
-class KarateClubDataset(DGLDataset):
+class HelsinkiDataset(DGLDataset):
     def __init__(self):
-        super().__init__(name='karate_club')
+        super().__init__(name='Helsinki')
 
     def process(self):
-        nodes_data = pd.read_csv('./members.csv')
-        edges_data = pd.read_csv('./interactions.csv')
-        node_features = torch.from_numpy(nodes_data['Age'].to_numpy())
-        node_labels = torch.from_numpy(nodes_data['Club'].astype('category').cat.codes.to_numpy())
-        edge_features = torch.from_numpy(edges_data['Weight'].to_numpy())
-        edges_src = torch.from_numpy(edges_data['Src'].to_numpy())
-        edges_dst = torch.from_numpy(edges_data['Dst'].to_numpy())
+        nodes_data = pd.read_csv('./SCG_Dataset/OSMNode/LasVegas.csv', low_memory=False,encoding='cp949')
+        edges_data = pd.read_csv('./SCG_Dataset/OSMEdge/Edge_LasVegas.csv', low_memory=False,encoding='cp949')
+        node_features = torch.from_numpy(nodes_data['lon'].to_numpy())
+        # node_labels = torch.from_numpy(nodes_data['Club'].astype('category').cat.codes.to_numpy())
+        node_labels=torch.from_numpy(nodes_data['lat'].to_numpy())
+        edge_features = torch.from_numpy(edges_data['distance'].to_numpy())
+        edges_src = torch.from_numpy(edges_data['src_id'].to_numpy())
+        edges_dst = torch.from_numpy(edges_data['dst_id'].to_numpy())
 
         self.graph = dgl.graph((edges_src, edges_dst), num_nodes=nodes_data.shape[0])
         self.graph.ndata['feat'] = node_features
@@ -39,12 +35,15 @@ class KarateClubDataset(DGLDataset):
         n_nodes = nodes_data.shape[0]
         n_train = int(n_nodes * 0.6)
         n_val = int(n_nodes * 0.2)
+
         train_mask = torch.zeros(n_nodes, dtype=torch.bool)
         val_mask = torch.zeros(n_nodes, dtype=torch.bool)
         test_mask = torch.zeros(n_nodes, dtype=torch.bool)
+
         train_mask[:n_train] = True
         val_mask[n_train:n_train + n_val] = True
         test_mask[n_train + n_val:] = True
+
         self.graph.ndata['train_mask'] = train_mask
         self.graph.ndata['val_mask'] = val_mask
         self.graph.ndata['test_mask'] = test_mask
@@ -55,7 +54,8 @@ class KarateClubDataset(DGLDataset):
     def __len__(self):
         return 1
 
-dataset = KarateClubDataset()
+
+dataset = HelsinkiDataset()
 graph = dataset[0]
 
 print(graph)

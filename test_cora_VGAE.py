8f263f88622fd329be2d23b94042a529335ab548
SeongraeNoh <rhosunr99@khu.ac.kr>
2022-10-21 15:25:41 +0900

    test_cora_VGAE file updata: for encoder of total architecture, GAT is selected. in this file, encoder part's scheme has implemented ( node classification process of Cora Dataset) but this version of implementation doesn't attend to edge feature, need to be updated in future task

--- test_cora_VGAE.py
+++ test_cora_VGAE.py
@@ -5,26 +5,56 @@ import numpy as np
 import os
 import dgl
 import dgl.function as fn
-from dgl.nn.pytorch.conv import EGATConv
-import args
+from dgl.nn.pytorch.conv import GATConv
 import dgl.data
-from dgl.nn.pytorch.conv import EGATConv
 
-dataset = dgl.data.TUDataset('AIDS')
-print('Number of categories:', dataset.num_classes)
-print(len(dataset))
+dataset = dgl.data.CoraGraphDataset()
+g = dataset[0]
+
 
 class GCN(nn.Module):
-    def __init__(self, in_node_feats, in_edge_feats, out_node_feats, out_edge_feats, num_heads, num_classes):
+    def __init__(self, in_feat, out_feat, num_classes, num_heads):
         super(GCN, self).__init__()
-        self.conv1 = EGATConv(in_node_feats, in_edge_feats, out_node_feats, out_edge_feats, num_heads)
-        self.conv2 = EGATConv(out_node_feats, out_edge_feats,num_classes,1,num_heads)
+        self.conv1 = GATConv(in_feat, out_feat, num_heads)
+        self.conv2 = GATConv(out_feat*num_heads, num_classes, 1,)
 
     def forward(self, g, in_feat):
         h = self.conv1(g, in_feat)
-        h = F.relu(h)
+        h = h.view(-1, h.size(1) * h.size(2))  # (in_feat, num_heads, out_dim) -> (in_feat, num_heads * out_dim)
+        # WHAT?
+        h = F.elu(h)
         h = self.conv2(g, h)
+        h = h.squeeze()  # (in_feat, 1, out_dim) -> (in_feat, out_dim)
         return h
 
-# Create the model with given dimensions
-# model = GCN(g.ndata['feat'].shape[1], 16, dataset.num_classes)
+
+def train(g, model):
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+    best_val_acc = 0
+    best_test_acc = 0
+
+    features = g.ndata['feat']
+    labels = g.ndata['label']
+    train_mask = g.ndata['train_mask']
+    val_mask = g.ndata['val_mask']
+    test_mask = g.ndata['test_mask']
+    for e in range(100):
+        # Forward
+        logits = model(g, features)
+
+        # Compute prediction
+        logp = F.log_softmax(logits, 1)
+        loss = F.nll_loss(logp[train_mask], labels[train_mask])
+        # Backward
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if e % 5 == 0:
+            print("Epoch {:05d} | Loss {:.4f} ".format(
+                e, loss.item()))
+
+
+g = g.to('cuda')
+model = GCN(g.ndata['feat'].shape[1], 16, dataset.num_classes, 3).to('cuda')
+train(g, model)
